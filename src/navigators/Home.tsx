f383d43e1ccb6bb409576f8c975802bc069a5ddd
mmilad75 <55688834+mmilad75@users.noreply.github.com>
2022-02-12 19:02:35 +0330

    refactor bottom tabbar

--- src/navigators/Home.tsx
+++ src/navigators/Home.tsx
@@ -17,9 +17,9 @@ const Tab = createBottomTabNavigator<HomeTabParamsList>();
 
 const HomeTab: React.FC = () => (
 	<Tab.Navigator>
-		<Tab.Screen options={{headerTitle: 'Best Seller'}} name="home.bestSeller" component={BestSellers} />
-		<Tab.Screen options={{headerTitle: 'Shopping List'}} name="home.shoppingList" component={ShoppingList} />
-		<Tab.Screen options={{headerTitle: 'Profile'}} name="home.profile" component={Profile} />
+		<Tab.Screen options={{headerTitle: 'Best Seller', tabBarIcon: () => null, tabBarLabel: 'Best Seller'}} name="home.bestSeller" component={BestSellers} />
+		<Tab.Screen options={{headerTitle: 'Shopping List', tabBarIcon: () => null, tabBarLabel: 'Shopping List'}} name="home.shoppingList" component={ShoppingList} />
+		<Tab.Screen options={{headerTitle: 'Profile', tabBarIcon: () => null, tabBarLabel: 'Profile'}} name="home.profile" component={Profile} />
 	</Tab.Navigator>
 );
 
